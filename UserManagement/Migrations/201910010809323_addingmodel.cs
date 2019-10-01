21535b666c71955f82cd8615abcd80bb523fa0f9
Heriwadana0806 <herywardana2892@gmail.com>
2019-10-01 15:38:35 +0700

    First User Management

---
+++ UserManagement/Migrations/201910010809323_addingmodel.cs
@@ -0,0 +1,186 @@
+﻿namespace UserManagement.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addingmodel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tb_M_Application",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Contract",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Status = c.Boolean(nullable: false),
+                        JoinDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Department",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_District",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Division",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_T_Employee_Application",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_T_Employee_Role",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Employee",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        First_Name = c.String(),
+                        Last_Name = c.String(),
+                        Blood_Type = c.String(),
+                        Gender = c.String(),
+                        Phone_Number = c.String(),
+                        Marital_Status = c.String(),
+                        Address = c.String(),
+                        Zip_Code = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Position",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Province",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Religion",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Role",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Subdistrict",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_T_User",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        Password = c.String(),
+                        Verified_Email = c.Boolean(nullable: false),
+                        Token = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tb_M_Village",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Tb_M_Village");
+            DropTable("dbo.Tb_T_User");
+            DropTable("dbo.Tb_M_Subdistrict");
+            DropTable("dbo.Tb_M_Role");
+            DropTable("dbo.Tb_M_Religion");
+            DropTable("dbo.Tb_M_Province");
+            DropTable("dbo.Tb_M_Position");
+            DropTable("dbo.Tb_M_Employee");
+            DropTable("dbo.Tb_T_Employee_Role");
+            DropTable("dbo.Tb_T_Employee_Application");
+            DropTable("dbo.Tb_M_Division");
+            DropTable("dbo.Tb_M_District");
+            DropTable("dbo.Tb_M_Department");
+            DropTable("dbo.Tb_M_Contract");
+            DropTable("dbo.Tb_M_Application");
+        }
+    }
+}
