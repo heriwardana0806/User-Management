3e5917633b44d58a8ad97135b1adbdf04b76d14e
Heriwadana0806 <herywardana2892@gmail.com>
2019-10-01 19:16:12 +0700

    I create sign in

---
+++ UserManagement/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UserManagement.Context;
+using UserManagement.Models;
+
+namespace UserManagement.Controllers
+{
+    public class UserController
+    {
+        public void AddUser(string email, string pword)
+        {
+            var result = 0;
+            User user = new User();
+            MyContext _context = new MyContext();
+
+            user.Email = email;
+            user.Password = pword;
+            _context.Users.Add(user);
+            result = _context.SaveChanges();
+
+            if (result > 0)
+            {
+                MessageBox.Show("Register Successful!");
+            }
+            else
+            {
+                MessageBox.Show("Register Failed!");
+            }
+        }
+
+        public void UserLogin(string email, string pword)
+        {
+
+            User user = new Models.User();
+            MyContext _context = new MyContext();
+
+            var get = _context.Users.Where(u => u.Email == email).FirstOrDefault<User>();
+
+            if (get == null)
+            {
+                MessageBox.Show("You are not Registered yet!");
+            }
+            else
+            {
+                if (get.Password != pword)
+                {
+                    MessageBox.Show("Your Password is Incorrect!");
+                }
+                else
+                {
+                    MessageBox.Show("Login Successful!");
+                }
+            }
+        }
+
+        public void ChangePass(string email, string pword)
+        {
+            var result = 0;
+
+            User user = new User();
+            MyContext _context = new MyContext();
+
+            var get = _context.Users.Where(u => u.Email == email).FirstOrDefault<User>();
+
+            if (get == null)
+            {
+                MessageBox.Show("Your Username is Incorrect!");
+            }
+            else
+            {
+                if (get.Password != pword)
+                {
+                    get.Password = pword;
+                    result = _context.SaveChanges();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Update Success!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Update Failed!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Your Password can't be the same!");
+                }
+            }
+        }
+    }
+}
--- UserManagement/Views/Login.xaml.cs
+++ UserManagement/Views/Login.xaml.cs
@@ -10,7 +10,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserManagement.Controllers;
+using UserManagement.Views;
 
 namespace UserManagement.Views
 {
@@ -22,6 +25,50 @@ namespace UserManagement.Views
         public Login()
         {
             InitializeComponent();
+
+            UsernameTextBox.Text = Properties.Settings.Default.Email;
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            UserController CallUser = new UserController();
+
+            if (UsernameTextBox.Text.Length == 0 && PasswordTextBox.Password.Length == 0)
+            {
+                UsernameErrorMessage.Text = "You Must Enter Valid Email!";
+                PasswordErrorMessage.Text = "You Must Enter Password!";
+                PasswordTextBox.Focus();
+                UsernameTextBox.Focus();
+            }
+            else if (UsernameTextBox.Text.Length == 0)
+            {
+                UsernameErrorMessage.Text = "You Must Enter Valid Email!";
+                UsernameTextBox.Focus();
+            }
+            else if (PasswordTextBox.Password.Length == 0)
+            {
+                PasswordErrorMessage.Text = "You Must Enter Password!";
+                PasswordTextBox.Focus();
+            }
+            else
+            {
+                string email = UsernameTextBox.Text;
+                string password = PasswordTextBox.Password;
+
+                CallUser.UserLogin(email, password);
+                this.Hide();
+                Home home = new Home();
+                home.Show();
+            }
+        }
+
+
+        
+
+        private void RememberCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Email = UsernameTextBox.Text;
+            Properties.Settings.Default.Save();
         }
     }
 }
